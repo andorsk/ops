4c6a71711d8b4f31b6bca4122a9a5ea6dea8d63a
andorsk <andor.developer@gmail.com>
2017-10-30 16:10:22 -0700

    added a few other scripts and changed directory structure

---
+++ ops/photos_api.py
@@ -0,0 +1,18 @@
+import gdata.photos.services
+import gdata.media
+import gdata.geo
+
+
+import gdata.docs.service
+
+# Create a client class which will make HTTP requests with Google Docs server.
+client = gdata.docs.service.DocsService()
+# Authenticate using your Google Docs email address and password.
+client.ClientLogin('andor.main@gmail.com', 'c0raline')
+
+# Query the server for an Atom feed containing a list of your documents.
+documents_feed = client.GetDocumentListFeed()
+# Loop through the feed and extract each document entry.
+for document_entry in documents_feed.entry:
+  # Display the title of the document on the command line.
+  print document_entry.title.text
