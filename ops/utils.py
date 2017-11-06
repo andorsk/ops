543d41aecdf6dae621ceb319445fe0d797b7357a
andorsk <andor.developer@gmail.com>
2017-11-06 13:40:57 -0800

    added mp42srt

--- ops/utils.py
+++ ops/utils.py
@@ -2,6 +2,11 @@ import argparse
 import sys
 import os
 
+if len(sys.argv) < 2:
+    print("Welcome to the utility cli for your computer")
+    print("Please 'help' command function to see available commands")
+    exit()
+
 command = sys.argv[1]
 
 
@@ -16,26 +21,35 @@ def img2s3():
     command = ('aws s3 cp {} s3://BUCKET_NAME/images/sj/'.format(args.file))
     os.system(command)
 
-def command2():
-    print("COMMAND 2")
+def mp42srt():
+    if args.file is None:
+        print("Please make sure to specify file")
+        exit()
+    command = ('python2.7 /usr/local/bin/autosub {} -K {} -D en -S en -F srt').format(args.file, os.environ['GOOGLE_AK']) 
+    print("Running command " + command)
+    os.system(command)
 
 def help():
+    print("Commands: ")
     for key in HELP:
         print(key, ':', HELP[key])
 
 
 HELP = {
     'img2s3' : 'Push an image into the ss3 henosisknot bucket',
+    'mp42srt' : 'Convert an mp4 file into an srt file',
     'help' : 'Get Helpful information'
 }
 
 
 FUNCTION_MAP = {
     'img2s3': img2s3,
+    'mp42srt': mp42srt,
     'help': help
 }
 
 
 
 run = FUNCTION_MAP[command]
+
 run()
